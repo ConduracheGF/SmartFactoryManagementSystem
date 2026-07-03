39e8fc1bbe19d090a3a8aedcb92cfd98e38f5867
ConduracheGF <condurachegabriel14@gmail.com>
2026-07-03 12:54:58 +0300

    Folder de diagrame

--- ElectronicsFactory/ElectronicsFactory/obj/Debug/net10.0/ElectronicsFactory.AssemblyInfo.cs
+++ ElectronicsFactory/ElectronicsFactory/obj/Debug/net10.0/ElectronicsFactory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectronicsFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65bebb38abbca2a7acd4d6a5276a118d536d7746")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031e499fe1287e908b75cea5b2cf9e592f7156f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectronicsFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectronicsFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
